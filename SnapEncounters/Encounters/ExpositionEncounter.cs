9358cc6afb7f271fc60d1b98848f8ee5975e427e
haxmjl <haxmjl@localhost>
2013-08-24 11:44:28 -0700

    Move Encounters classes. Create simple background for game.

---
+++ SnapEncounters/Encounters/ExpositionEncounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spiridios.SnapEncounters.Encounters
+{
+    public class ExpositionEncounter : Encounter
+    {
+        List<String> exposition = new List<String>();
+
+        public ExpositionEncounter()
+            : base()
+        {
+        }
+
+        public ExpositionEncounter(String expositionLine)
+            : base()
+        {
+            this.AddLine(expositionLine);
+        }
+
+        public ExpositionEncounter AddLine(String expositionLine)
+        {
+            this.exposition.Add(expositionLine);
+            return this;
+        }
+
+    }
+}
