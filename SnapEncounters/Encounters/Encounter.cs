fa3660d062e3e0a17e0302104cfd4673091b1b85
haxmjl <1332337+Spiridios@users.noreply.github.com>
2013-08-24 11:44:28 -0700

    Move Encounters classes. Create simple background for game.

---
+++ SnapEncounters/Encounters/Encounter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Spiridios.SnapEncounters.Encounters
+{
+    public class Encounter
+    {
+        private Encounter nextEncounter;
+
+        public Encounter NextEncounter
+        {
+            get { return nextEncounter; }
+            set { this.nextEncounter = value; }
+        }
+    }
+}
